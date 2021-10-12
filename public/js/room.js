4b45297979a4ba3574a674937f45fe373091045a
Harsh Mange <harshmange44@gmail.com>
2021-10-12 18:06:57 +0530

    .

--- public/js/room.js
+++ public/js/room.js
@@ -206,10 +206,7 @@ const configuration = {
     iceServers: [
         { urls: "stun:stun.stunprotocol.org" }, 
         { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:stun1.l.google.com:19302' },
-        { urls: 'stun:stun2.l.google.com:19302' },
-        { urls: 'stun:stun3.l.google.com:19302' },
-        { urls: 'stun:stun4.l.google.com:19302' }
+        { urls: 'stun:stun1.l.google.com:19302' }
     ]
  }
 
