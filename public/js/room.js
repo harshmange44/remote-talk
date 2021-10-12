b40675c4fe11ef8a0d4f9f8294782f7dd64b9ac2
Harsh Mange <harshmange44@gmail.com>
2021-10-12 17:29:12 +0530

    .

--- public/js/room.js
+++ public/js/room.js
@@ -821,7 +821,7 @@ videoButt.addEventListener('click', () => {
         }
         videoButt.innerHTML = `<i class="fas fa-video"></i>`;
         videoAllowed = 1;
-        videoButt.style.backgroundColor = "#4ECCA3";
+        videoButt.style.backgroundColor = "#6f9cf0";
         if (mystream) {
             mystream.getTracks().forEach(track => {
                 if (track.kind === 'video')
@@ -863,7 +863,7 @@ audioButt.addEventListener('click', () => {
         }
         audioButt.innerHTML = `<i class="fas fa-microphone"></i>`;
         audioAllowed = 1;
-        audioButt.style.backgroundColor = "#4ECCA3";
+        audioButt.style.backgroundColor = "#6f9cf0";
         if (mystream) {
             mystream.getTracks().forEach(track => {
                 if (track.kind === 'audio')
